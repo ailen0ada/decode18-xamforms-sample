0611c2405f5a4aa00111d1b5d181aa38a39916fa
Tsubasa HIRANO <t.hirano@fluffy-bunnys.jp>
2018-05-20 18:27:01 +0900

    implemented simple resizing method

--- XamForms.DesktopSample/XamForms.DesktopSample.Core/MainPage.xaml.cs
+++ XamForms.DesktopSample/XamForms.DesktopSample.Core/MainPage.xaml.cs
@@ -24,6 +24,10 @@ namespace XamForms.DesktopSample.Core
             this.ImageView.Source = ImageSource.FromFile(file.FullName);
 
             var colors = PaletteExtractor.Extract(file, 4);
+            this.ColorTip1.Color = colors[0];
+            this.ColorTip2.Color = colors[1];
+            this.ColorTip3.Color = colors[2];
+            this.ColorTip4.Color = colors[3];
         }
     }
 }
--- XamForms.DesktopSample/XamForms.DesktopSample.Core/PaletteExtractor.cs
+++ XamForms.DesktopSample/XamForms.DesktopSample.Core/PaletteExtractor.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.IO;
 using Xamarin.Forms;
+using SkiaSharp;
+using System.Linq;
 
 namespace XamForms.DesktopSample.Core
 {
@@ -8,7 +10,14 @@ namespace XamForms.DesktopSample.Core
     {
         public static Color[] Extract(FileInfo file, int paletteLength)
         {
-            return Array.Empty<Color>();
+            using (var bmp = SKBitmap.Decode(file.FullName))
+            using(var scaled = bmp.Resize(new SKImageInfo(paletteLength / 2, paletteLength / 2), SKBitmapResizeMethod.Box))
+            {
+                return scaled.Pixels
+                             .Take(paletteLength)
+                             .Select(x => Color.FromRgba(x.Red, x.Green, x.Blue, x.Alpha))
+                             .ToArray();
+            }
         }
     }
 }
