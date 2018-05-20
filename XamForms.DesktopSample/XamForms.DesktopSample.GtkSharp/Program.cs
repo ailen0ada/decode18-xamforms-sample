9a75f51ad19bbef14439c35fb6614fc310f8af9d
Tsubasa HIRANO <t.hirano@fluffy-bunnys.jp>
2018-05-20 15:22:02 +0900

    add GtkSharp project

---
+++ XamForms.DesktopSample/XamForms.DesktopSample.GtkSharp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Gtk;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.GTK;
+
+namespace XamForms.DesktopSample.GtkSharp
+{
+    class MainClass
+    {
+		[STAThread]
+        public static void Main(string[] args)
+        {
+            Gtk.Application.Init();
+			Forms.Init();
+
+			var app = new Core.App();
+			var window = new FormsWindow();
+			window.LoadApplication(app);
+			window.Show();
+
+            Gtk.Application.Run();
+        }
+    }
+}
